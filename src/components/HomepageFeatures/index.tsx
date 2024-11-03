f279b66e15b49a3e9c0bf3d66ce2e87ce8db7822
Icemic <bingfeng.web@gmail.com>
2024-11-03 23:32:34 +0800

    Updates feature text

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,70 +1,80 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
+	title: string;
+	Svg: React.ComponentType<React.ComponentProps<"svg">>;
+	description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+	{
+		title: "一致的跨平台能力",
+		Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+		description: (
+			<>
+				支持各种桌面平台、移动平台和 Web。
+				<br />
+				“一次编写，各处运行”。
+			</>
+		),
+	},
+	{
+		title: "高度自定义的界面",
+		Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+		description: (
+			<>
+				使用 React 开发你想要的任何界面。
+				<br />
+				对接成熟社区的海量资源和开发人员。
+			</>
+		),
+	},
+	{
+		title: "渐进式和灵活性",
+		Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+		description: (
+			<>对于不同的用户，提供不同的使用方式，从简单的模板到完全自定义。</>
+		),
+	},
+	{
+		title: "开源且商业友好",
+		Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+		description: (
+			<>
+				基于 MPL-2.0 开源协议。
+				<br />
+				免费使用，更可以用于商业用途。
+			</>
+		),
+	},
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, Svg, description }: FeatureItem) {
+	return (
+		<div className={clsx("col col--3")}>
+			<div className="text--center">
+				<Svg className={styles.featureSvg} role="img" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<Heading as="h3">{title}</Heading>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
